641cbb0f13bcf0e4aebb1cf70d30698f0203c874
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-03-18 14:45:40 -0700

    Add Signup functionality and basic UI update in LoginActivity

--- app/src/main/java/com/bcit/calories_tracker/LoginActivity.java
+++ app/src/main/java/com/bcit/calories_tracker/LoginActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,11 +20,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private String mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
+        mode = "Login";
 
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -32,6 +36,44 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             Log.d("DEBUG/LoginActivity", "Not Login");
             setContentView(R.layout.activity_login);
+            updateLoginSignupUI();
+        }
+    }
+
+    // Switch login/sign up UI
+    public void updateLoginSignupUI() {
+        EditText cfPasswordEditText = findViewById(R.id.cfpassword_login_value);
+        TextView cfPasswordTextView = findViewById(R.id.cfpassword_login_label);
+        TextView navigateTextView = findViewById(R.id.navigate_login_text);
+        Button button = findViewById(R.id.button_login);
+
+        if (mode.equals("Login")) {
+            cfPasswordEditText.setVisibility(View.GONE);
+            cfPasswordTextView.setVisibility(View.GONE);
+            navigateTextView.setText(R.string.login_singup_navigate_text);
+            button.setText(R.string.login_login_button);
+        } else {
+            cfPasswordEditText.setVisibility(View.VISIBLE);
+            cfPasswordTextView.setVisibility(View.VISIBLE);
+            navigateTextView.setText(R.string.login_login_navigate_text);
+            button.setText(R.string.login_signup_button);
+        }
+    }
+
+    public void onClickSwitchLoginSignUp(View view) {
+        if (mode.equals("Login")) {
+            mode = "SignUp";
+        } else {
+            mode = "Login";
+        }
+        this.updateLoginSignupUI();
+    }
+
+    public void onClickButton(View view) {
+        if (mode.equals("Login")) {
+            this.onClickSignIn(view);
+        } else {
+            this.onClickSignUp(view);
         }
     }
 
@@ -48,44 +90,65 @@ public class LoginActivity extends AppCompatActivity {
 
         // Sign In using email and password
         mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("DEBUG/LoginActivity", "signInWithEmail:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        finish();
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("DEBUG/LoginActivity", "signInWithEmail:failure", task.getException());
-                        Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show();
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("DEBUG/LoginActivity", "signInWithEmail:success");
+                            finish();
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (Exception e) {
+                                String message = e.getMessage() + ". Please try again!";
+                                Toast.makeText(LoginActivity.this, message,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
                     }
-                }
-            });
+                });
     }
 
-    /**
-     * Sign UP Firebase
-     mAuth.createUserWithEmailAndPassword("test@test.com", "12345678")
-     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-    @Override
-    public void onComplete(@NonNull Task<AuthResult> task) {
-    if (task.isSuccessful()) {
-    // Sign in success, update UI with the signed-in user's information
-    Log.d("DEBUG/LoginActivity", "createUserWithEmail:success");
-    FirebaseUser user = mAuth.getCurrentUser();
-    Log.d("DEBUG/LoginActivity", user.getEmail());
-    //                        updateUI(user);
-    } else {
-    // If sign in fails, display a message to the user.
-    Log.d("DEBUG/LoginActivity", "createUserWithEmail:failure", task.getException());
-    Toast.makeText(LoginActivity.this, "Authentication failed.",
-    Toast.LENGTH_SHORT).show();
-    //                        updateUI(null);
+    public void onClickSignUp(View view) {
+        EditText emailEditText = findViewById(R.id.email_login_value);
+        EditText passwordEditText = findViewById(R.id.password_login_value);
+        EditText cfPasswordEditText = findViewById(R.id.cfpassword_login_value);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String cfPassword = cfPasswordEditText.getText().toString();
+
+        // Do nothing if email, password, or confirm password empty
+        if (email.trim().equals("") || password.trim().equals("") || cfPassword.trim().equals("")) {
+            return;
+        }
+
+        // Do nothing if email or password empty
+        if (!password.equals(cfPassword)) {
+            Toast.makeText(LoginActivity.this,
+                    "Password and Confirm Password are not matched. Please try again!",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Sign In using email and password
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("DEBUG/LoginActivity", "createUserWithEmail:success");
+                        } else {
+                            try {
+                                throw task.getException();
+                            } catch (Exception e) {
+                                String message = e.getMessage() + ". Please try again!";
+                                Toast.makeText(LoginActivity.this, message,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
     }
-    }
-    });
-     */
 }
--- app/src/main/java/com/bcit/calories_tracker/MainActivity.java
+++ app/src/main/java/com/bcit/calories_tracker/MainActivity.java
@@ -4,20 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Force Sign Out, remove later
-        FirebaseAuth.getInstance().signOut();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        if (currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+            this.updateUI();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        this.updateUI();
+    }
+
+    public void updateUI() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            TextView textView = findViewById(R.id.textView_main);
+            if (textView != null) {
+                textView.setText("Welcome " + currentUser.getEmail());
+            }
+        }
+    }
+
+    public void onClickLogout(View view) {
+        mAuth.signOut();
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
