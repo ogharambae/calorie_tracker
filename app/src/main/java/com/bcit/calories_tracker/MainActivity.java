e93a1cbc7bd34aff7df8f9e487a8ec5ea475ac9a
Huynh Nhan Ngo <ngohuynhnhan@gmail.com>
2022-03-22 01:01:03 -0700

    Add navigation drawer with signout function item

--- app/src/main/java/com/bcit/calories_tracker/MainActivity.java
+++ app/src/main/java/com/bcit/calories_tracker/MainActivity.java
@@ -1,19 +1,28 @@
 package com.bcit.calories_tracker;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth mAuth;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +36,18 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         } else {
+
             this.updateUI();
         }
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onRestart() {
@@ -38,20 +56,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateUI() {
-        Log.d("DEBUG/MainActivity", "here");
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null) {
-            Log.d("DEBUG/MainActivity", "here inside");
-            TextView textView = findViewById(R.id.textView_main);
-            if (textView != null) {
-                textView.setText("Welcome " + currentUser.getEmail());
-            }
+
+        if (currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return;
         }
+
+        this.configureDrawer();
+
+        // Do stuff inside here
+//            Log.d("DEBUG/MainActivity", "here inside");
+//            TextView textView = findViewById(R.id.textView_main);
+//            if (textView != null) {
+//                textView.setText("Welcome " + currentUser.getEmail());
+//            }
     }
 
-    public void onClickLogout(View view) {
-        mAuth.signOut();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+    public void configureDrawer() {
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.main);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Attach click event to drawer items
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView_main);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        if (item.getItemId() == R.id.item_drawer_logout) {
+            mAuth.signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+        //close navigation drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
