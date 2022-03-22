0698689565e4e7dd340d098b1a0546319b6498d3
HyunBae Choi <hyunbaechoi93@gmail.com>
2022-03-22 16:03:07 -0700

    Updat Meal class

--- app/src/main/java/com/bcit/calories_tracker/Meal.java
+++ app/src/main/java/com/bcit/calories_tracker/Meal.java
@@ -4,13 +4,19 @@ import java.io.Serializable;
 
 public class Meal implements Serializable {
     String name;
+    String cal;
+    String carb;
+    String fat;
+    String protein;
     int quantity;
-    int calories;
 
-    public Meal(String name, int quantity, int calories) {
+    public Meal(String name, String cal, String carb, String fat, String protein, int quantity) {
         this.name = name;
+        this.cal = cal;
+        this.carb = carb;
+        this.fat = fat;
+        this.protein = protein;
         this.quantity = quantity;
-        this.calories = calories;
     }
 
     public String getName() {
@@ -21,19 +27,43 @@ public class Meal implements Serializable {
         this.name = name;
     }
 
+    public String getCal() {
+        return cal;
+    }
+
+    public void setCal(String cal) {
+        this.cal = cal;
+    }
+
+    public String getCarb() {
+        return carb;
+    }
+
+    public void setCarb(String carb) {
+        this.carb = carb;
+    }
+
+    public String getFat() {
+        return fat;
+    }
+
+    public void setFat(String fat) {
+        this.fat = fat;
+    }
+
+    public String getProtein() {
+        return protein;
+    }
+
+    public void setProtein(String protein) {
+        this.protein = protein;
+    }
+
     public int getQuantity() {
         return quantity;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
-    }
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
     }
 }
